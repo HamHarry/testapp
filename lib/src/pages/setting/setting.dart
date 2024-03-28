67a2cfb0fd746be7914d9393e151079297c91523
HamHarry <hamza_2641@hotmail.com>
2024-03-28 14:27:51 +0700

    TestSliver

--- lib/src/pages/setting/setting.dart
+++ lib/src/pages/setting/setting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testapp/src/pages/data/data.dart';
 
 class Settingpage extends StatefulWidget {
   const Settingpage({super.key});
@@ -8,10 +9,32 @@ class Settingpage extends StatefulWidget {
 }
 
 class _SettingpageState extends State<Settingpage> {
+  final List<DataModel> _mockUp = [
+    DataModel("HamHarry", "2.4 Ghz", true),
+    DataModel("Neng", "5 Ghz", false),
+    DataModel("TTXPZ", "2.4 Ghz", false),
+    DataModel("GGEZ", "2.4 Ghz", true),
+    DataModel("BU", "2.4 Ghz", true),
+    DataModel("BUMAIL", "5 Ghz", false),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(children: [Text("EZ")]),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              ((context, index) {
+                return Container(child: Text(_mockUp[index].name));
+              }),
+              childCount: _mockUp.length,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Text("ez"),
+          )
+        ],
+      ),
     );
   }
 }
