35a830744e2a6a6113d634f3f1c563dc8490831c
HamHarry <hamza_2641@hotmail.com>
2024-03-31 01:02:54 +0700

    Test_Dropdown

--- lib/src/pages/setting/setting.dart
+++ lib/src/pages/setting/setting.dart
@@ -8,19 +8,55 @@ class Settingpage extends StatefulWidget {
 }
 
 class _SettingpageState extends State<Settingpage> {
+  String? selectedValue;
+  List<String> dropdownItem = ["Item 1", "Item 2", "Item 3"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff101F3D),
       body: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.red,
-            width: 3.0,
-            style: BorderStyle.solid,
-          ),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xff3E5888),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  child: DropdownButton<String>(
+                    hint: const Text("Select Mode"),
+                    value: selectedValue,
+                    items: dropdownItem.map((String value) {
+                      return DropdownMenuItem<String>(
+                          value: value, child: Text(value));
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        selectedValue = newValue;
+                      });
+                    },
+                    isExpanded: true,
+                    dropdownColor: Color(0xff3E5888),
+                    borderRadius: BorderRadius.circular(20),
+                    icon: const Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Color(0xff87A2D3),
+                    ),
+                    iconSize: 20,
+                    underline: Container(),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        child: Text('ตัวอย่าง'),
       ),
     );
   }
